c9c9d7b00772e24224515120bd5c2470b7e2df41
jvictory225@gmail.com <joseph>
2024-05-01 16:55:14 +0100

    new fix

--- symposium/symposium/settings.py
+++ symposium/symposium/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2j6icmuz1_$0z-^zpzq=7)%*%w9y2#xi4duqox7vd$hvzt7sm2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
